f5a1ffbac58646e99714c5972c3aa1d4cdd9b4db
iamtoyosee <123267125+iamtoyosee@users.noreply.github.com>
2024-12-11 00:01:39 -0600

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class SwapCommand extends UndoCommand {
+    private ArrayList<String> sourceList;
+    private int index1;
+    private int index2;
+
+    public SwapCommand(ArrayList<String> lst, int idx1, int idx2) {
+        this.sourceList = lst;
+        this.index1 = idx1;
+        this.index2 = idx2;
+    }
+
+    @Override
+    public void execute() {
+        // This swaps the items at index 1 and index 2 by using a temporary variable to
+        // hold the value.
+        if (index1 >= 0 && index1 < sourceList.size() && index2 >= 0 && index2 < sourceList.size()) {
+            String temp = sourceList.get(index1);
+            sourceList.set(index1, sourceList.get(index2));
+            sourceList.set(index2, temp);
+        }
+    }
+}
